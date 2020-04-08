145eb04820b1c14c923a4a308fada36903f6d071
Xiangkun Yang <vipakun@hotmail.com>
2020-04-07 22:40:56 -0500

    only export function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import nav from './nav';
 import {top, bottom} from './footer';
 import makeButton from './botton';
-import {peru, azure, makeColorSytle} from './button-styles';
+import { makeColorSytle } from './button-styles';
 
 console.log(nav(), top, bottom, makeButton('THIS IS A BUTTON'), makeColorSytle('cyan'));
