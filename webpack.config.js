b28d8e65536ee96daa957230298ac3d29fc88def
Xiangkun Yang <vipakun@hotmail.com>
2020-04-08 00:08:18 -0500

    added built-in plugin and external plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = (env) => {
   return {
     mode: env.mode,
     output: {
       filename: "bundle.js",
     },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new webpack.ProgressPlugin((percentage, message) => {
+        console.log(`${(percentage * 100).toFixed()}% ${message}`);
+      }),
+    ],
   };
 };
