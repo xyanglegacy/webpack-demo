3ac695aa0d2cd809668754e95a9b4cb32b9fcc3e
Xiangkun Yang <vipakun@hotmail.com>
2020-04-23 22:46:59 -0500

    fixed typo

--- src/button-styles.js
+++ src/button-styles.js
@@ -4,5 +4,5 @@ const makeColorStyle = (color) => `color: ${color};`;
 
 exports.peru = peru;
 exports.azure = azure;
-exports.makeColorSytle = makeColorSytle;
-// export {peru, azure, makeColorStyle}
+exports.makeColorStyle = makeColorStyle;
+// export { peru, azure, makeColorStyle };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import nav from './nav';
-import {top, bottom} from './footer';
-import makeButton from './botton';
-import { makeColorStyle } from './button-styles';
+import nav from "./nav";
+import { top, bottom } from "./footer";
+import makeButton from "./botton";
+import { makeColorStyle } from "./button-styles";
 
-console.log(nav(), top, bottom, makeButton('THIS IS A BUTTON'), makeColorSytle('cyan'));
+console.log(
+  nav(),
+  top,
+  bottom,
+  makeButton("THIS IS A BUTTON"),
+  makeColorStyle("cyan")
+);
