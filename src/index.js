411c087211669e18833942d823b9741112e900c9
Xiangkun Yang <vipakun@hotmail.com>
2020-05-02 00:16:11 -0500

    use code splitting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import nav from "./nav";
-import { footer } from "./footer";
+// 3rd
+// import { footer } from "./footer";
+const getFooter = () => import("./footer");
 import makeButton from "./button";
 import { makeColorSytle } from "./button-styles";
 import makeImage from "./image";
@@ -12,7 +14,22 @@ import buttonStyle from "./button.css";
 const button = makeButton("YAH!");
 button.style = makeColorSytle("peru");
 document.body.appendChild(button);
-document.body.appendChild(footer);
+
+// 3rd
+button.addEventListener("click", (e) => {
+  getFooter().then((footerModule) => {
+    document.body.appendChild(footerModule.footer);
+  });
+});
+
+// 2nd
+// naive way. still loading the code.
+// button.addEventListener("click", (e) => {
+//   document.body.appendChild(footer);
+// });
+
+// 1st
+// document.body.appendChild(footer);
 
 const image = makeImage(logoUrl);
 document.body.appendChild(image);
